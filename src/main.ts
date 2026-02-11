a52cc80e6046d158a111e4e20521dd53aeb5f95b
anandkr06 <tech.anandkr06@gmail.com>
2026-02-11 21:05:41 +0530

    changes are done port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   // });
   app.enableCors({ origin: '*' });
   //await app.listen(4000);
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
