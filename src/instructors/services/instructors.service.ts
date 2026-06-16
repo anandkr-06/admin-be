819aeec56ef25406af8ff2a75b2e3b4a1a9e9231
anandkr06 <tech.anandkr06@gmail.com>
2026-06-16 18:08:42 +0530

    changes added

--- src/instructors/services/instructors.service.ts
+++ src/instructors/services/instructors.service.ts
@@ -184,7 +184,7 @@ export class InstructorsService {
           name: { $concat: ['$firstName', ' ', '$lastName'] },
           stripeAccountId: 1,
           email: 1,
-          mobile: 1,
+          mobileNumber: 1,
           isActive: 1,
           isPublish: 1,
           createdAt: 1,
